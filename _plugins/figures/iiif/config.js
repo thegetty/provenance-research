75f494ed7cce58fdd900db4abcb15706a80e396d
Greg Albers <galbers@getty.edu>
2026-05-11 13:34:47 -0700

    Enlarge default inline image width to match container

--- _plugins/figures/iiif/config.js
+++ _plugins/figures/iiif/config.js
@@ -117,7 +117,7 @@ export default (eleventyConfig) => {
       {
         name: 'static-inline-figure-image',
         resize: {
-          width: 626
+          width: 720
         }
       }
     ]
